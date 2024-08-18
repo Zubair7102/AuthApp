c4a492a3a51f20352e063b925939f97f00fcfe95
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-19 01:59:36 +0530

    token + JWT

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,6 +3,8 @@ const userData = require("../models/userData");
 // importing the json web token JWT
 const jwt = require("jsonwebtoken");
 
+require("dotenv").config();
+
 
 // signup
 exports.signup = async(req, res) => {
@@ -86,12 +88,40 @@ exports.login = async (req, res) => {
             })
         }
 
+
+        const payload = {
+            email: user.email,
+            id: user._id,
+            role: user.role,
+        }
+
         // verify password and generate a JWT token
         if(await bcrypt.compare(password, user.password))
         {
             // password matched
-            let token = jwt.sign();
+            let token = jwt.sign(payload,
+                            process.env.JWT_SECRET,
+                        {
+                            expiresIn: "2h",
+                        });
 
+            // putting the created token into the user
+            user.token = token;
+            user.password = undefined;
+            const options = {
+                expiresIn: new Date( Date.now() + 3*24 * 60 * 60 * 1000),
+                httpOnly: true,
+                // htpOnly means that the cookie can not be accessed at the client side 
+            }
+
+            // cookie takes 3 parameters cookie name, cookie data, and some options of your choice
+            // here cookie name is token and we are pssing the above created token as cookie data  
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token, 
+                user,
+                message: 'User Logged in Successfully',
+            })
         }
         else{
             // password did not matched
@@ -103,6 +133,11 @@ exports.login = async (req, res) => {
     }
     catch(error)
     {
-
+        console.error(error)
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: 'Failed to Login : /',
+        })
     }
 }
