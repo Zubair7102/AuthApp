40f4a1f1366fa0812f1587b90b441a1e2f1c33ee
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-18 23:36:11 +0530

    first commit

---
+++ controllers/Auth.js
@@ -0,0 +1,56 @@
+const bcrypt = require("bcrypt");
+const userData = require("../models/userData");
+const userData = require("../models/userData");
+
+
+exports.signup = async(req, res) => {
+    try{
+        // getting the data from the request
+        const {name, email, password, role} = req.body;
+
+        // check if the user already exist or not 
+        const existingUser = await userData.find({email});
+
+        if(existingUser)
+        {
+            return res.status(400).json({
+                success: false,
+                message: "User Already exists ",
+            })
+        }
+
+
+        // secure the password
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password, 10);
+            // using the above line of code we will encrypt the password using 10 rounds
+        }
+        catch(error)
+        {
+            return res.status(500).json({
+                success: false,
+                message: 'Error in encrypting(hashing)'
+            });
+        }
+
+        // reate entry for new user
+        const user = userData.create({
+            name, email, password:hashedPassword, role
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: 'User created successfully',
+        });
+    }
+    catch(error)
+    {
+        console.error(error)
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message: "User cannot be registered, please try again later"
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+const dbConnect = require("./config/database")
+dbConnect();
+
+// route import and mount 
+const user = require("./routes/user");
+
+app.use("/api/v1", user);
+
+
+// activate
+app.listen(PORT, () => {
+    console.log(`APP is listening at ${PORT}`)
+})
