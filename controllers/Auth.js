7557f3f0429daaac57c1ea6e6b13297dee5799a2
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-19 01:30:13 +0530

    jwt config

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,8 +1,10 @@
 const bcrypt = require("bcrypt");
 const userData = require("../models/userData");
+// importing the json web token JWT
+const jwt = require("jsonwebtoken");
 
 
-
+// signup
 exports.signup = async(req, res) => {
     try{
         // getting the data from the request
@@ -52,5 +54,55 @@ exports.signup = async(req, res) => {
             success:false,
             message: "User cannot be registered, please try again later"
         })
+    }
+}
+
+
+// login
+
+exports.login = async (req, res) => {
+    try{
+        // fetch the data from the req body
+        const {email, password} = req.body
+        // validate email and password if they have been entered or not 
+        if(!email || !password)
+        {
+            return res.status(400).json({
+                success: false,
+            message: 'Please fill the details carefully'
+            });
+        
+        }
+
+        // check if the user is available or not in the db 
+        const user = await userData.findOne({email});
+
+        // if not a registered user
+        if(!user )
+        {
+            return res.status(401).json({
+                success: false,
+                message: 'Please signup as you are not a registered user',
+            })
+        }
+
+        // verify password and generate a JWT token
+        if(await bcrypt.compare(password, user.password))
+        {
+            // password matched
+            let token = jwt.sign();
+
+        }
+        else{
+            // password did not matched
+            return res.status(402).json({
+                success: false,
+                message: 'Password did not matched the given email'
+            })
+        }
+    }
+    catch(error)
+    {
+
     }
 }
